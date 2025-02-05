94e6ceed674169ebffbb296fb0e9060a6ef64740
simha10 <simhachalammandapudi@gmial.com>
2025-02-05 09:41:16 +0530

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex items-center justify-center w-full h-10 py-3 bg-purple-950 text-grey-200'>
+<div className='flex flex-col items-center justify-center w-full py-3 bg-purple-950 text-grey-200 sm:h-auto'>
+
       <div>
-        Developed and Designed by <span className='text-green-200 font-bold'>@simhachalam Mandapudi</span> and <span className='text-red-200 font-bold'>@Srinivas Matta</span>
+        Developed and Designed by <span className='text-sm sm:text-base md:text-lg text-green-200 font-bold'>@simhachalam Mandapudi</span> and <span className='text-sm sm:text-base md:text-lg text-red-200 font-bold'>@Srinivas Matta</span>
+
       </div>
     </div>
   )
